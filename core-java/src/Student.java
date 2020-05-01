079873af0382a80287e60b0609adf4b708d5810b
jbm20123 <jbm20123@gmail.com>
2020-05-01 18:32:19 +0530

    lecture-02(java 01st program)

---
+++ core-java/src/Student.java
@@ -0,0 +1,12 @@
+
+class Student {
+	int age=16;
+	int id=123;
+	String name="Swarna";
+	void read() {
+	System.out.println("I am reading a book");
+	}
+	void speak() {
+	System.out.println("I can speak english");
+	}
+}
---
+++ core-java/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+public static void main(String[]args) {
+	Student stu=new Student();
+	stu.read();
+	stu.speak();
+	System.out.println(stu.name);
+	System.out.println(stu.age);
+	System.out.println(stu.id);
+}
+}
